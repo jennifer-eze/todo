9a543a0ba04ea425f52fe18d751b93f5070af434
Ogochukwu Eze <ogochukwu.eze@student.griffith.ie>
2020-10-06 13:12:40 +0100

    Delete requests

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -6,11 +6,19 @@ from .serializers import TodoSerializer
 
 class TodoList(APIView):
     def get(self, request):
-        return Response(None)
-        
+        todos = Todo.objects.all()
+        todo_data = TodoSerializer(todos, many = True)
+        return Response(todo_data.data)
+
     def post(self, request):
-        todo = TodoSerializer(request.data)
+        todo = TodoSerializer(data=request.data)
+        if todo.is_valid():
+            todo.save()
         return Response(todo.data)
+
+    def delete(self, request):
+        Todo.objects.all().delete()
+        return Response(None)
 
 class TodoOne(APIView):
     pass
