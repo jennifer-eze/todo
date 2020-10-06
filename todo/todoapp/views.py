06dc041929d9cae47f0929fccdcc4f3e51f1275d
Ogochukwu Eze <ogochukwu.eze@student.griffith.ie>
2020-10-06 15:59:20 +0100

    handling error cases

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -9,36 +9,46 @@ class TodoList(APIView):
     def get(self, request):
         todos = Todo.objects.all()
         todo_data = TodoSerializer(todos, many = True)
-        return Response(todo_data.data)
+        return Response(todo_data.data, status=200)
 
     def post(self, request):
-        todo = TodoSerializer(data=request.data)
-        if todo.is_valid():
-            todo_item = todo.save()
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            todo_item = serializer.save()
             todo_item.completed = False
             todo_item.url = reverse('todo_one', args=[todo_item.id], request=request)
             todo_item.save()
-        return Response(todo.data)
+            return Response(serializer.data, status=201)
+        return Response(None, status=400)
 
     def delete(self, request):
         Todo.objects.all().delete()
-        return Response(None)
+        return Response(None, status=204)
 
 class TodoOne(APIView):
     def get(self, request, todo_id):
-        todo = Todo.objects.get(pk=todo_id)
-
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
+        try:
+            todo = Todo.objects.get(pk=todo_id)
+            serializer = TodoSerializer(todo)
+            return Response(serializer.data, status=200)
+        except Todo.DoesNotExist:
+            return Response(None, status=400)
 
     def patch(self, request, todo_id):
-        todo = Todo.objects.get(pk=todo_id)
-        serializer = TodoSerializer(data=request.data, instance=todo, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
+        try:
+            todo = Todo.objects.get(pk=todo_id)
+            serializer = TodoSerializer(data=request.data, instance=todo, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=200)
+            return Response(None, status=400)
+        except Todo.DoesNotExist:
+            return Response(None, status=400)
 
     def delete(self, request, todo_id):
-        todo = Todo.objects.get(pk=todo_id)
-        todo.delete()
-        return Response(None)
+        try:
+            todo = Todo.objects.get(pk=todo_id)
+            todo.delete()
+            return Response(None, status=204)
+        except Todo.DoesNotExist:
+            return Response(None, status=400)
