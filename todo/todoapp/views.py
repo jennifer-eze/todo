0e820f012e3cb487d279ed0e6a04f590848d8bee
Ogochukwu Eze <ogochukwu.eze@student.griffith.ie>
2020-10-06 11:58:48 +0100

    creating views and url

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class TodoList(APIView):
+    def post(self, request):
+        todo = TodoSerializer(request.data)
+        return Response(todo.data)
+
+class TodoOne(APIView):
+    pass
+
